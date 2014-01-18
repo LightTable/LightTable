2b96338e61769327d119f2ee8db7e79f2b555754
Gunnar Ahlberg <github@gunnarahlberg.com>
2014-01-18 11:17:10 +0100

    Check lein version. LT does not build on older lein versions

--- linux_deps.sh
+++ linux_deps.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # Check if lein is installed
 lein version >/dev/null 2>&1 || { echo >&2 "Please install leiningen before running this script."; exit 1; }
+if [ "$(echo `lein version` | grep 'Leiningen 1.\|2.0')" ]; then 
+	echo "lein version must be 2.1 or above. Do a lein upgrade first"; exit 1;
+fi
 
 echo "### Fetching binaries ###"
 BITS=""
