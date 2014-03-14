e2f2d1c05ea1bb56d55c20201995669e07678d9e
Gabriel Horner <gabriel.horner@gmail.com>
2014-03-14 08:31:04 -0400

    allow options to be set for line commenting

--- src/lt/objs/editor/pool.cljs
+++ src/lt/objs/editor/pool.cljs
@@ -248,14 +248,26 @@
 ;; Editor commands
 ;;****************************************************
 
+;; See https://github.com/marijnh/CodeMirror/blob/master/addon/comment/comment.js for options
+(def line-comment-options {})
+
+(behavior ::line-comment-options
+          :triggers #{:object.instant}
+          :type :user
+          :desc "Editor: Set options for line commenting"
+          :params [{:label "map"
+                    :example "{:indent true}"}]
+          :reaction (fn [this options]
+                      (set! line-comment-options options)))
+
 (cmd/command {:command :comment-selection
               :desc "Editor: Comment line(s)"
               :exec (fn []
                       (when-let [cur (last-active)]
                         (let [cursor (editor/->cursor cur "start")]
                           (if (editor/selection? cur)
-                            (editor/line-comment cur cursor (editor/->cursor cur "end"))
-                            (editor/line-comment cur cursor cursor)))))})
+                            (editor/line-comment cur cursor (editor/->cursor cur "end") line-comment-options)
+                            (editor/line-comment cur cursor cursor line-comment-options)))))})
 
 (cmd/command {:command :uncomment-selection
               :desc "Editor: Uncomment line(s)"
@@ -275,7 +287,7 @@
                                             [cursor (editor/->cursor cur "end")]
                                             [cursor cursor])]
                           (when-not (editor/uncomment cur start end)
-                            (editor/line-comment cur cursor (editor/->cursor cur "end"))))))})
+                            (editor/line-comment cur cursor (editor/->cursor cur "end") line-comment-options)))))})
 
 (cmd/command {:command :indent-selection
               :desc "Editor: Indent line(s)"
