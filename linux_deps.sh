ef3bf67675f22a365c6f042459c49fd9653d51ce
Rostislav Svoboda <thebost@gmail.com>
2014-05-11 12:54:02 +0200

    linux_deps.sh: Re-use downloaded LightTableLinux*.gz, clojure-plugin
    * download LightTableLinux*.tar.gz only if it doesn't exist and always
      verify its sha1 checksum.
    * store clojure-plugin as a git-submodule and clone it only once. Then
      update it with git pull --rebase

--- linux_deps.sh
+++ linux_deps.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+buildVer=0.6.0
+
 # Check if lein is installed
 lein version >/dev/null 2>&1 || { echo >&2 "Please install leiningen before running this script."; exit 1; }
 if [ "$(echo `lein version` | grep 'Leiningen 1.\|2.0')" ]; then 
-	echo "lein version must be 2.1 or above. Do a lein upgrade first"; exit 1;
+	echo "ERROR: lein version must be 2.1 or above. Do a lein upgrade first"; exit 1;
 fi
 
 which curl &> /dev/null
@@ -13,25 +15,60 @@ if [ $retCode -ne 0 ]; then
     exit $retCode
 fi
 
-echo "### Fetching binaries ###"
-BITS=""
-if [ $(getconf LONG_BIT) == "64" ]; then BITS="64"; fi
-TARBALL=LightTableLinux$BITS.tar.gz
-curl -O https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/0.6.0/$TARBALL
-tar -xzf $TARBALL
-rm $TARBALL
-cp -ar deploy/* LightTable
-rm -rf deploy
-mv LightTable deploy
+# Use linux-specific directories not to break non-linux builds by any dumb coicidence.
+deployDir=deploy
+binariesDir=binaries
 
-echo "### Building cljs ###"
+echo "INFO: Fetching binaries"
+
+bits_cpu=$(getconf LONG_BIT)  # bits_cpu can be 32 or 64
+bits_tarball=${bits_cpu//32}  # bits_tarball can be 64 or undefined
+
+node_webkit_tarball=$binariesDir/node-webkit/LightTableLinux$bits_cpu-$buildVer.tar.gz
+if [ ! -e "$node_webkit_tarball" ]; then
+    curl -o $node_webkit_tarball \
+	https://d35ac8ww5dfjyg.cloudfront.net/playground/bins/$buildVer/LightTableLinux$bits_tarball.tar.gz
+fi
+sha1sum --check $node_webkit_tarball.sha1
+retCode=$?
+if [ $retCode -ne 0 ]; then
+    exit $retCode
+fi
+
+tar -xzf $node_webkit_tarball
+
+# $deployDir doesn't exist when building for the 1st time
+if [ -e $deployDir ]; then
+    cp -ar $deployDir/* LightTable
+    rm -rf $deployDir
+fi
+mv LightTable $deployDir
+
+echo "INFO: Building cljs"
 lein cljsbuild clean && lein cljsbuild once
+#lein cljsbuild once
 
-echo "### Fetching clojure plugin ###"
-cd deploy/plugins
-rm -rf clojure
-git clone https://github.com/LightTable/Clojure.git clojure
+echo "INFO: Building clojure plugin"
+workingDir=$(pwd)
+clojureDir=$binariesDir/clojure
 
-echo "### Building clojure plugin ###"
-cd clojure
+cd $clojureDir
+
+# $clojureDir/.git doesn't exist when building for the 1st time
+if [ -e .git ]; then
+    # TODO add runner/target/lein-light-standalone.jar to the .gitignore
+    # in the https://github.com/LightTable/Clojure.git repository
+    git checkout runner/target/lein-light-standalone.jar
+    git pull --rebase
+else
+    git submodule init
+    git submodule update
+
+    # We're on a detached branch now. We need to...
+    git checkout master
+    # ...otherwise git won't know what to pull on consecutive builds.
+    # See the if-then block above.
+fi
+cp -r . $workingDir/$deployDir/plugins/
 ./build.sh
+# cd $workingDir
