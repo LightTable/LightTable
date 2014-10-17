57a8d89b150a0c1d63eebef65a77186348267d67
Boryslav Larin <brabadu@gmail.com>
2014-10-17 23:52:20 +0300

    Fixed status_bar.cljs warnings

--- src/lt/objs/status_bar.cljs
+++ src/lt/objs/status_bar.cljs
@@ -94,6 +94,8 @@
 (defn ->cursor-str [{:keys [pos]}]
   [:span.pos (str "" (inc (:line pos)) " / " (inc (:ch pos)))])
 
+(declare status-cursor)
+
 (behavior ::update-cursor-location
                   :triggers #{:update!}
                   :reaction (fn [this pos]
@@ -156,6 +158,9 @@
                         (status-item (log this) "left")
                         ))
 
+(def status-loader (object/create ::status.loader))
+(add-status-item status-loader)
+
 (defn loader-set []
   (object/merge! status-loader {:loaders 0}))
 
@@ -165,9 +170,6 @@
 (defn loader-dec []
   (if (> (:loaders @status-loader) 0)
     (object/update! status-loader [:loaders] dec)))
-
-(def status-loader (object/create ::status.loader))
-(add-status-item status-loader)
 
 ;;**********************************************************
 ;; console list
@@ -182,6 +184,14 @@
   :click (fn []
            (cmd/exec! :toggle-console)))
 
+(object/object* ::status.console-toggle
+                :dirty 0
+                :tags [:status.console-toggle]
+                :init (fn [this]
+                        (status-item (toggle-span this) "")))
+
+(def console-toggle (object/create ::status.console-toggle))
+(add-status-item console-toggle)
 
 (defn dirty []
   (object/update! console-toggle [:dirty] inc))
@@ -192,12 +202,3 @@
 
 (defn console-class [class]
   (object/merge! console-toggle {:class class}))
-
-(object/object* ::status.console-toggle
-                :dirty 0
-                :tags [:status.console-toggle]
-                :init (fn [this]
-                        (status-item (toggle-span this))))
-
-(def console-toggle (object/create ::status.console-toggle))
-(add-status-item console-toggle)
