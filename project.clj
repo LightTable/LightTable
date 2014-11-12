429cde6f60c58cc8d443a5146cba84edb4d4d7e9
Vlad-Ștefan Harbuz <vlad@vladh.net>
2014-11-12 20:41:37 +0000

    Remove noir dependency (I don't think it's needed)

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://www.lighttable.com/"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [crate "0.2.5"]
-                 [noir "1.3.0"]
                  ;[core.async "0.1.0-SNAPSHOT"]
                  [org.clojure/clojurescript "0.0-2138"
                   :exclusions [org.apache.ant/ant]]
