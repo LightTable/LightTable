f3cb0f07ae83e0472813e5b29e839865a4401452
Gabriel Horner <gabriel.horner@gmail.com>
2014-12-09 18:17:13 -0500

    Correct fix for paths with a dot in the directory #1755

--- src/lt/objs/files.cljs
+++ src/lt/objs/files.cljs
@@ -88,9 +88,7 @@
         (recur (rest parts) (conj acc (string/join "." parts)))))))
 
 (defn ext [path]
-  (let [i (.lastIndexOf path ".")]
-    (when (> i 0)
-      (subs path (inc i) (count path)))))
+  (subs (.extname fpath path) 1))
 
 (defn without-ext [path]
   (let [i (.lastIndexOf path ".")]
