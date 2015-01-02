7bfb2f65427182294c4582bb6bf1234daa193791
Gabriel Horner <gabriel.horner@gmail.com>
2015-01-02 11:58:21 -0500

    Revert back to older atom-shell where remote-debugging works
    This allows console to work again.
    In b5b1c142b6d92d3f254c93aa43432230d4d31e14 we upgraded to an
    atom-shell where remote-debugging /json doesn't seem to work.
    Details at atom/atom-shell#969

--- shell/Gruntfile.js
+++ shell/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     "download-atom-shell": {
-      version: "0.20.3",
+      version: "0.19.5",
       outputDir: "./atom-shell",
       rebuild: true
     }
