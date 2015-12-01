c99c0b77240a2cfb740e518435df196eea307e1e
Gabriel Horner <gabriel.horner@gmail.com>
2015-12-01 01:06:17 -0500

    Upgrade to Electron 0.34.5
    Held off on upgrading to 0.35.0 since there ipc changes to handle

--- deploy/electron/Gruntfile.js
+++ deploy/electron/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     "download-electron": {
-      version: "0.31.1",
+      version: "0.34.5",
       outputDir: "./electron",
       rebuild: true
     }
