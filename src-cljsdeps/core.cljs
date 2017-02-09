e15b8437459f31d51013dc8a7df23b6f32235eda
Scott Bauer <sbauer322@gmail.com>
2017-02-09 18:53:10 -0500

    1973-update-clojurescript: 1973-update-clojurescript: 2017-01-29
    Misc formatting and typo updates

--- src-cljsdeps/core.cljs
+++ src-cljsdeps/core.cljs
@@ -1,7 +1,7 @@
 (ns cljsdeps.core
-  "ClojureScript used to build cljsDeps.js as describe in
+  "ClojureScript used to build cljsDeps.js as described in
   https://github.com/LightTable/LightTable/commit/8e73f59891c45f73a1b985fed69795d1061a8ecf#commitcomment-10073128.
-  Generated javascript is used in background thread with worker-thread object."
+  Generated Javascript is used in background thread with worker-thread object."
   (:require [clojure.string :as string]
             [clojure.walk :as walk]
             [cljs.reader :as reader]))
