88a7445160acf2415927bbdee35b137918ac05eb
Pratik Karki <predatoramigo@gmail.com>
2019-03-14 22:21:08 +0545

    WIP: needs commit splitting
    Signed-off-by: Pratik Karki <predatoramigo@gmail.com>

--- deploy/core/node_modules/bencode/test/data.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    binKeyData: new Buffer("ZDU6ZmlsZXNkMjA6N7VVuuCjmp5LoM+n15a5iM/XJHdkODpjb21wbGV0ZWkwZTEwOmRvd25sb2FkZWRpMTBlMTA6aW5jb21wbGV0ZWkwZWVlZQ==", 'base64')
-  , binKeyName: (new Buffer("N++/vVXvv73go5rvv71L77+9z6fXlu+/ve+/ve+/ve+/vSR3", 'base64')).toString()
-}
