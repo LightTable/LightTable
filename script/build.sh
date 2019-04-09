183a584a801f48b20d504c777f3c4030994b0eb3
Pratik Karki <predatoramigo@gmail.com>
2019-04-09 21:31:50 +0545

    Bump electron and cljs to working versions where code inline eval still works
    Signed-off-by: Pratik Karki <predatoramigo@gmail.com>

--- script/build.sh
+++ script/build.sh
@@ -35,7 +35,7 @@ rm -f deploy/core/node_modules/lighttable/bootstrap.js
 lein cljsbuild once app
 
 # Fetch plugins
-PLUGINS=("Clojure,0.3.1" "CSS,0.0.6" "HTML,0.1.0" "Javascript,0.2.0"
+PLUGINS=("Clojure,0.3.3" "CSS,0.0.6" "HTML,0.1.0" "Javascript,0.2.0"
          "Paredit,0.0.4" "Python,0.0.7" "Rainbow,0.0.8")
 
 # Plugins cache
