b24727fabb21448e8771e072f8be6ec2716e52ad
Pratik Karki <predatoramigo@gmail.com>
2020-01-16 12:17:10 +0545

    Update electron version fixing security issues

--- deploy/electron/Gruntfile.js
+++ deploy/electron/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         "download-electron": {
-            version: "1.8.8",
+            version: "2.0.18",
             outputDir: "./electron",
             rebuild: true
         }
